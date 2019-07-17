73c61495c8af2f316b6cb6896cae7d9aa7201430
Adams Temidire <gr8temi@gmail.com>
2019-07-17 07:57:42 +0100

    add home link to signup and login page

--- Account/models.py
+++ Account/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-	username=models.CharField(max_length=50, default='User', unique=True)
+	# username=models.CharField(max_length=50, default='User', unique=True)
 	license=models.CharField(default='xxx-xxx-xxx',max_length=50)
 	picture = models.ImageField(upload_to="profile", default="profile/users.png")
 	is_active=models.BooleanField(default=True)
